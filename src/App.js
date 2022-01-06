1c860a7a3ade9c0c9ab60e33e10064352ceb3684
Anna An <arzumanyan.an@gmail.com>
2022-01-06 16:04:39 +0400

    Added mobile icons and some functionality

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 // we import this without the index.js part because it's called index.js, and there is no need for that.
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
