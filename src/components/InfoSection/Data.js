ce713af30ac73ebfc1b45a1dc6ef4299aa939288
Anna An <arzumanyan.an@gmail.com>
2022-01-18 12:12:52 +0400

    Styling the discover page

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,13 +1,13 @@
 // by importing the svg like this and using it below worked
 import imgUrl1 from "../../images/svg-1.svg";
-import imgUrl2 from "../../images/svg-2.svg";
+import imgUrl4 from "../../images/svg-4.svg";
 import imgUrl3 from "../../images/svg-3.svg";
 
 export const homeObjOne = {
   id: "about",
   lightBg: true,
   lightText: false,
-  lightTextDesc: true,
+  lightTextDesc: false,
   topLine: "Premium Bank",
   headline: "Unlimited Transactions with zero fees",
   description:
@@ -16,39 +16,39 @@ export const homeObjOne = {
   imgStart: false,
   img: imgUrl1,
   alt: "Car",
-  dark: true,
-  primary: true,
-  darkText: false,
+  dark: false,
+  primary: false,
+  darkText: true,
 };
 
 export const homeObjTwo = {
-  id: "about",
+  id: "discover",
   lightBg: true,
   lightText: false,
-  lightTextDesc: true,
-  topLine: "Premium Bank",
-  headline: "Unlimited Transactions with zero fees",
+  lightTextDesc: false,
+  topLine: "unlimited Access",
+  headline: "Login to your account at any time",
   description:
-    "Get access to our exclusive app that allows you to send unlimited transactioins without getting charged any fees.",
-  buttonLabel: "Get started",
-  imgStart: false,
-  img: imgUrl2,
-  alt: "Car",
-  dark: true,
-  primary: true,
-  darkText: false,
+    "We have you covered no matter where you are located. All you need is an internet connection and a phone or computer",
+  buttonLabel: "Learn more",
+  imgStart: true,
+  img: imgUrl4,
+  alt: "Document",
+  dark: false,
+  primary: false,
+  darkText: true,
 };
 
 export const homeObjThree = {
-  id: "about",
+  id: "signup",
   lightBg: true,
   lightText: false,
-  lightTextDesc: true,
+  lightTextDesc: false,
   topLine: "Premium Bank",
   headline: "Unlimited Transactions with zero fees",
   description:
     "Get access to our exclusive app that allows you to send unlimited transactioins without getting charged any fees.",
-  buttonLabel: "Get started",
+  buttonLabel: "Start now",
   imgStart: false,
   img: imgUrl3,
   alt: "Car",
