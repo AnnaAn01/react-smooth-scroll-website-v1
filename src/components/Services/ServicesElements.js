3a8e3be021df2364cb81155944e4ed2f8b00b6e5
Anna An <arzumanyan.an@gmail.com>
2022-01-24 11:58:08 +0400

    styling the services section

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -16,3 +16,39 @@ export const ServicesContainer = styled.div`
     height: 1300px;
   }
 `;
+export const ServicesWrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-column: 1fr 1fr 1fr;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 50px;
+
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
+  }
+`;
+
+export const ServicesCard = styled.div`
+  background: #fff;
+  display: flex;
+  flex-direction: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+`;
