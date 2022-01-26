502b0763ca1fe31fe3db2f1b803c040adb72ee0f
Anna An <arzumanyan.an@gmail.com>
2022-01-26 19:13:32 +0400

    utilizing React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // we import this without the index.js part because it's called index.js, and there is no need for that.
 import Home from "./pages";
+import SigninPage from "./pages/Signin";
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
     </Router>
   );
 }
